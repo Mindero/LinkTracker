b4039b1b8ca399bae674205e16ce1dbe37495b07
vadim <vadimyanchenko64@gmail.com>
2025-03-01 22:34:43 +0400

    SCRAPPER feat: добавил service

---
+++ scrapper/src/main/java/backend/academy/scrapper/service/ScrapperService.java
@@ -0,0 +1,40 @@
+package backend.academy.scrapper.service;
+
+import backend.academy.scrapper.exception.NotSuchSDKException;
+import backend.academy.scrapper.repo.LinkRepository;
+import backend.academy.scrapper.repo.Track;
+import backend.academy.scrapper.service.sdk.LinkSDK;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ScrapperService {
+    private final LinkRepository linkRepository;
+
+    private final List<LinkSDK> sdkList;
+
+    public ScrapperService(@Autowired LinkRepository linkRepository, @Autowired List<LinkSDK> sdkList) {
+        this.linkRepository = linkRepository;
+        this.sdkList = sdkList;
+    }
+
+    public void addUser(Long id){
+        linkRepository.addUser(id);
+    }
+    public void addTrackLink(Long id, Track track) throws NotSuchSDKException{
+        if (sdkList.stream().noneMatch(t -> t.validURL(track.link()))){
+            throw new NotSuchSDKException("Не удалось распарсить такую ссылку");
+        }
+        linkRepository.addTrack(id, track);
+    }
+    public void unTrack(Long id, String link){
+        linkRepository.unTrack(id, link);
+    }
+    public List<Link> getLinkList(Long id){
+        return linkRepository.getLinkList(id)
+            .stream()
+            .map(track -> new Link(id, track.link(), track.tags(), track.filters()))
+            .toList();
+    }
+}
